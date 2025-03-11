b0be520f616a4eab462066862b7ebde065e76ea8
GaetanBordes <bordes.gaetan33@gmail.com>
2025-03-11 14:24:15 +0100

    order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_11_130355) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_11_132349) do
   create_table "cart_items", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "product_id", null: false
@@ -19,6 +19,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_11_130355) do
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_cart_items_on_product_id"
     t.index ["user_id"], name: "index_cart_items_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.decimal "total_price"
+    t.string "status"
+    t.string "stripe_payment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -45,4 +55,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_11_130355) do
 
   add_foreign_key "cart_items", "products"
   add_foreign_key "cart_items", "users"
+  add_foreign_key "orders", "users"
 end
