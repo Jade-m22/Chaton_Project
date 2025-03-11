ae74482955c7898d85367c377007531dc91082f3
GaetanBordes <bordes.gaetan33@gmail.com>
2025-03-11 14:07:52 +0100

    model cart item

---
+++ app/models/cart_item.rb
@@ -0,0 +1,10 @@
+class CartItem < ApplicationRecord
+  belongs_to :user
+  belongs_to :product
+
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
+
+  def total_price
+    product.price * quantity
+  end
+end
