ca12b3919b7a13b13c17ca6b0b2aed185fd215f2
Jade-m22 <mj.micheljade@gmail.com>
2025-03-12 10:49:51 +0100

    panier ok, need to add quantity

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,7 +7,7 @@ class CartsController < ApplicationController
 
   def add_item
     product = Product.find(params[:product_id])
-    cart_item = @cart.cart_items.find_or_initialize_by(product: product)
+    cart_item = @cart.cart_items.find_or_initialize_by(product: product, user_id: current_user.id)
     cart_item.quantity ||= 0
     cart_item.quantity += 1
 
@@ -36,6 +36,6 @@ class CartsController < ApplicationController
   private
 
   def set_cart
-    @cart = current_user.cart || Cart.new(user: current_user)
+    @cart = current_user.cart || Cart.create(user: current_user)
   end
 end
