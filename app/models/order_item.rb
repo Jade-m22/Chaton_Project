75735d9507bab4ac2f2edb95c09a119b7c799cda
Jade-m22 <mj.micheljade@gmail.com>
2025-03-12 15:38:10 +0100

    add stripe

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,9 +3,9 @@ class OrderItem < ApplicationRecord
   belongs_to :product
 
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
-  validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   def total_price
-    unit_price * quantity
+    price * quantity
   end
 end
